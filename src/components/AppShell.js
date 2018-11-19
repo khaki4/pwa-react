8ae7e0add4d6594924eb7826fa15d438be65438e
pi3rrot <4dakto@gmail.com>
2018-11-19 16:53:42 +0900

    첫 frame 에서 할 일을 줄여 어플리케이션 레벨에서 사용되는 초기 구동 시간 줄이는 방법

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -1,7 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
 import { AppBar, Drawer, MenuItem } from 'material-ui';
+
+class SidebarDrawer extends Component {
+  componentDidMount() {
+    let frameCount = 0;
+    debugger
+    const mount = () => (frameCount++ > 0) ?
+      this.props.onMounted() : requestAnimationFrame(mount);
+
+    requestAnimationFrame(mount);
+  }
+  render() {
+  	return (
+  		<Fragment>
+        <Drawer
+          open={this.props.open}
+          docked={false}
+          onRequestChange={this.props.onRequestChange}
+        >
+          <MenuItem
+            primaryText="Home"
+            containerElement={<Link to="/" />}
+            onClick={this.props.onClick}
+          />
+          <MenuItem
+            primaryText="Users"
+            containerElement={<Link to="/users" />}
+            onClick={this.props.onClick}
+          />
+          <MenuItem
+            primaryText="Notification"
+            containerElement={<Link to="/notification" />}
+            onClick={this.props.onClick}
+          />
+        </Drawer>
+      </Fragment>
+  	);
+  }
+
+}
 
 export default class AppShell extends Component {
   constructor(props) {
@@ -9,46 +48,43 @@ export default class AppShell extends Component {
 
     this.state = {
       open: false,
+      drawer: false,
     };
   }
 
-  handleDrawerToggle = () => this.setState(() => ({ open: !this.state.open}))
+  handleDrawerToggle = (e) => {
+    if (this.state.drawer) {
+      this.setState(() => ({ open: !this.state.open}))
+    } else {
+      this.setState({drawer: true});
+      e.preventDefault();
+    }
+  }
 
-  handleRequestChange = (open) => this.setState(() => ({ open }))
+  handleRequestChange = (open) => () => this.setState(() => ({ open }))
 
   handleLinkclick = () => this.setState(() => ({ open: false }))
 
   render() {
+    const LasySidebarDrawer = this.state.drawer &&
+      <SidebarDrawer
+        open={this.state.open}
+        onMounted={this.handleRequestChange(true)}
+        onClick={this.handleRequestChange(false)}
+        onRequestChange={this.handleRequestChange(true)}
+      />
     return (
-      <div>
+      <Fragment>
         <MuiThemeProvider>
           <div>
             <AppBar onLeftIconButtonClick={this.handleDrawerToggle}/>
-            <Drawer open={this.state.open} docked={false}
-              onRequestChange={this.handleRequestChange}
-            >
-              <MenuItem
-                primaryText="Home"
-                containerElement={<Link to="/" />}
-                onClick={this.handleLinkclick}
-              />
-              <MenuItem
-                primaryText="Users"
-                containerElement={<Link to="/users" />}
-                onClick={this.handleLinkclick}
-              />
-              <MenuItem
-                primaryText="Notification"
-                containerElement={<Link to="/notification" />}
-                onClick={this.handleLinkclick}
-              />
-            </Drawer>
+            {LasySidebarDrawer}
             <div id="content" style={{width:"90%", margin: 'auto', marginTop: '30px'}}>
               { React.cloneElement(this.props.children) }
             </div>
           </div>
         </MuiThemeProvider>
-      </div>
+      </Fragment>
     )
   }
 }
